edd9d897919257ccef78bef8f741c8da4c0441dd
KacperM4 <kacper.marzeda@gmail.com>
2022-07-19 15:49:42 +0200

    deleted unnecesary coments

--- main.py
+++ main.py
@@ -16,33 +16,18 @@ def converttogray(image):
     cv2.imshow('M ',image)
     image = cv2.cvtColor(image, cv2.COLOR_GRAY2BGR)
 
-def mask(image):    mask = np.zeros(image.shape[:2], dtype="uint8")
-            # creating a rectangle on the mask
-      # where the pixels are valued at 255
-        # The kernel to be used for dilation purpose
+def mask(image):
+    mask = np.zeros(image.shape[:2], dtype="uint8")
     kernel = np.ones((5, 5), np.uint8)
-        
-    # converting the image to HSV format
     hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
-    #cv2.imshow('hsv', hsv)
-    # defining the lower and upper values of HSV,
-    # this will detect yellow colour
     Lower_hsv = np.array([20, 15, 15])
-    # creating the mask by eroding,morphing,
-    # dilating process
+    Upper_hsv = np.array([210, 210, 210])
     Mask = cv2.inRange(hsv, Lower_hsv, Upper_hsv)
     Mask = cv2.erode(Mask, kernel, iterations=1)
     Mask = cv2.morphologyEx(Mask, cv2.MORPH_OPEN, kernel)
     Mask = cv2.dilate(Mask, kernel, iterations=1)
-        
-    # Inverting the mask by
-    # performing bitwise-not operation
     mask = cv2.bitwise_not(Mask)
-  
-    # Displaying the image
     cv2.imshow("Mask", mask)
-
-           # performing a bitwise_and with the image and the mask
     masked = cv2.bitwise_and(image, image, mask=mask)
     cv2.imshow("Mask applied to Image", masked)
     return masked
