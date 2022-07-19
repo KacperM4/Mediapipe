44327b9d9a540a46bda24729be908e7a4eff8830
KacperM4 <kacper.marzeda@gmail.com>
2022-07-19 11:55:26 +0200

    Add points in mediapipe human skeletons which we are intrested in

--- main.py
+++ main.py
@@ -76,3 +76,11 @@ mp_drawing = mp.solutions.drawing_utils
 mp_drawing_styles = mp.solutions.drawing_styles
 mp_holistic = mp.solutions.holistic
 mp_pose = mp.solutions.pose
+
+TENNIS_POSE_CONNECTIONS = frozenset([(11, 12), (11, 13),
+                              (13, 15), (15, 17), (15, 19), (15, 21), (17, 19),
+                              (12, 14), (14, 16), (16, 18), (16, 20), (16, 22),
+                              (18, 20), (11, 23), (12, 24), (23, 24), (23, 25),
+                              (24, 26), (25, 27), (26, 28), (27, 29), (28, 30),
+                              (29, 31), (30, 32), (27, 31), (28, 32)])
+allPoints = []
