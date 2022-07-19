714928c6852231d8708540c1bf400dd67bd4f104
KacperM4 <kacper.marzeda@gmail.com>
2022-07-19 11:44:20 +0200

    no message

--- main.py
+++ main.py
@@ -21,9 +21,10 @@ def mask(image):    mask = np.zeros(image.shape[:2], dtype="uint8")
       # where the pixels are valued at 255
         # The kernel to be used for dilation purpose
     kernel = np.ones((5, 5), np.uint8)
-          
+        
     # converting the image to HSV format
-    hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)    # defining the lower and upper values of HSV,
+    hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+    #cv2.imshow('hsv', hsv)
+    # defining the lower and upper values of HSV,
     # this will detect yellow colour
     Lower_hsv = np.array([20, 15, 15])
-    Upper_hsv = np.array([210, 210, 210])
